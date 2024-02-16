67d96aaf4e289436bc08b292bba01454606adaeb
SuryaRajendr <73171944+SuryaRajendr@users.noreply.github.com>
2024-02-16 09:05:18 +0000

    hoisting added

---
+++ JS/hoisting.js
@@ -0,0 +1,18 @@
+
+// console.log(u) // ReferenceError: u is not defined
+
+c = 10
+var c ;
+console.log(c)
+
+
+// console.log(d)
+// const d = 5;
+
+console.log(sum (8,2))
+
+function sum(a,b) {
+    return a+b
+}
+
+console.log(sum (2,2))
